9407acab19c4eaa01708c8bae1eebad81a7a368d
AnnAngela <naganjue@vip.qq.com>
2022-08-26 23:17:15 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 DEBIAN_FRONTEND=noninteractive DEBIAN_PRIORITY=critical apt install -y -o "Dpkg::Options::=--force-confold" --no-install-recommends curl wget ca-certificates nano git libpam-systemd-
+apt autoremove --purge
 wget https://raw.githubusercontent.com/tj/n/master/bin/n -O /tmp/n
 bash /tmp/n lts
 rm /tmp/n
